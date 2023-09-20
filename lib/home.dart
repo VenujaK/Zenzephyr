c74c451530f84c5a735f56f9abbe8bd039b3f5a1
Venuja <93823328+VenujaK@users.noreply.github.com>
2023-09-21 03:17:46 +0530

    Home page done

--- lib/home.dart
+++ lib/home.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:zenzephyr/colors.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 
 class Home extends StatelessWidget {
   static const routeName = "/home";
-  const Home({Key? key});
 
   @override
   Widget build(BuildContext context) {
@@ -11,6 +12,7 @@ class Home extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
+          title: Text('Zenzephyr', style: TextStyle(color: AppColors.primaryColor)),
           iconTheme: IconThemeData(color: AppColors.primaryColor),
           backgroundColor: Colors.transparent,
           bottomOpacity: 0.0,
@@ -25,136 +27,226 @@ class Home extends StatelessWidget {
         body: Padding(
           padding: const EdgeInsets.all(8.0),
           child: GridView.count(
+            mainAxisSpacing: 10,
             crossAxisCount: 2,
             children: [
               Card(
-                  elevation: 4,
-                  shadowColor: const Color(0xFF00BF63),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                  child: ListView(
-                    padding: const EdgeInsets.all(8),
-                    children: [
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      Container(
-                        height: 65,
-                        child: Image.asset('assets/Chat with me.png'),
-                      ),
-                      Container(
-                        height: 30,
-                        child: const Center(child: Text('Chat With Me')),
-                      ),
-                      Container(
-                        height: 40,
-                        child: const Center(
-                            child: Text(
+                elevation: 4,
+                shadowColor: const Color(0xFF00BF63),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: ListView(
+                  padding: const EdgeInsets.all(8),
+                  children: [
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      height: 65,
+                      child: Image.asset('assets/Chat with me.png'),
+                    ),
+                    Container(
+                      height: 30,
+                      child: const Center(child: Text('Chat With Me')),
+                    ),
+                    Container(
+                      height: 40,
+                      child: const Center(
+                        child: Text(
                           'Engage in Enlightened Conversations',
                           textAlign: TextAlign.center,
                           style: TextStyle(
-                              color: AppColors.textColor, fontSize: 12),
-                        )),
-                      )
-                    ],
-                  )),
+                            color: AppColors.textColor,
+                            fontSize: 12,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
               Card(
-                  elevation: 4,
-                  shadowColor: const Color(0xFF00BF63),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                  child: ListView(
-                    padding: const EdgeInsets.all(8),
-                    children: [
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      Container(
-                        height: 65,
-                        child: Image.asset('assets/Talk with me.png'),
-                      ),
-                      Container(
-                        height: 30,
-                        child: const Center(child: Text('Talk To Me')),
-                      ),
-                      Container(
-                        height: 20,
-                        child: const Center(
-                            child: Text(
+                elevation: 4,
+                shadowColor: const Color(0xFF00BF63),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: ListView(
+                  padding: const EdgeInsets.all(8),
+                  children: [
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      height: 65,
+                      child: Image.asset('assets/Talk with me.png'),
+                    ),
+                    Container(
+                      height: 30,
+                      child: const Center(child: Text('Talk To Me')),
+                    ),
+                    Container(
+                      height: 20,
+                      child: const Center(
+                        child: Text(
                           'Expressions Unveiled',
                           textAlign: TextAlign.center,
                           style: TextStyle(
-                              color: AppColors.textColor, fontSize: 12),
-                        )),
-                      )
-                    ],
-                  )),
+                            color: AppColors.textColor,
+                            fontSize: 12,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
               Card(
-                  elevation: 4,
-                  shadowColor: const Color(0xFF00BF63),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                  child: ListView(
-                    padding: const EdgeInsets.all(8),
-                    children: [
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      Container(
-                        height: 65,
-                        child: Image.asset('assets/Contact us.png'),
-                      ),
-                      Container(
-                        height: 30,
-                        child: const Center(child: Text('Contact Us')),
-                      ),
-                      Container(
-                        height: 40,
-                        child: const Center(
-                            child: Text(
+                elevation: 4,
+                shadowColor: const Color(0xFF00BF63),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: ListView(
+                  padding: const EdgeInsets.all(8),
+                  children: [
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      height: 65,
+                      child: Image.asset('assets/Contact us.png'),
+                    ),
+                    Container(
+                      height: 30,
+                      child: const Center(child: Text('Contact Us')),
+                    ),
+                    Container(
+                      height: 40,
+                      child: const Center(
+                        child: Text(
                           'Connect for our Assistance',
                           textAlign: TextAlign.center,
                           style: TextStyle(
-                              color: AppColors.textColor, fontSize: 12),
-                        )),
-                      )
-                    ],
-                  )),
+                            color: AppColors.textColor,
+                            fontSize: 12,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
               Card(
-                  elevation: 4,
-                  shadowColor: const Color(0xFF00BF63),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                  child: ListView(
-                    padding: const EdgeInsets.all(8),
-                    children: [
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      Container(
-                        height: 65,
-                        child: Image.asset('assets/Calm me1.png'),
-                      ),
-                      Container(
-                        height: 30,
-                        child: const Center(child: Text('Calm Me')),
-                      ),
-                      Container(
-                        height: 40,
-                        child: const Center(
-                            child: Text(
+                elevation: 4,
+                shadowColor: const Color(0xFF00BF63),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: ListView(
+                  padding: const EdgeInsets.all(8),
+                  children: [
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      height: 65,
+                      child: Image.asset('assets/Calm me1.png'),
+                    ),
+                    Container(
+                      height: 30,
+                      child: const Center(child: Text('Calm Me')),
+                    ),
+                    Container(
+                      height: 40,
+                      child: const Center(
+                        child: Text(
                           'Calm yourself',
                           textAlign: TextAlign.center,
                           style: TextStyle(
-                              color: AppColors.textColor, fontSize: 12),
-                        )),
-                      )
-                    ],
-                  )),
+                            color: AppColors.textColor,
+                            fontSize: 12,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Card(
+                elevation: 4,
+                shadowColor: const Color(0xFF00BF63),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: ListView(
+                  padding: const EdgeInsets.all(8),
+                  children: [
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      height: 65,
+                      child: Image.asset('assets/SympathyBot1.png'),
+                    ),
+                    Container(
+                      height: 30,
+                      child: const Center(child: Text('SympathyBot')),
+                    ),
+                    Container(
+                      height: 40,
+                      child: const Center(
+                        child: Text(
+                          'Calm yourself',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: AppColors.textColor,
+                            fontSize: 12,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              
+              Card(
+                elevation: 4,
+                shadowColor: const Color(0xFF00BF63),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: ListView(
+                  padding: const EdgeInsets.all(8),
+                  children: [
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      height: 65,
+                      child: Image.asset('assets/SympathyBot1.png'),
+                    ),
+                    Container(
+                      height: 30,
+                      child: const Center(child: Text('Servey')),
+                    ),
+                    Container(
+                      height: 40,
+                      child: const Center(
+                        child: Text(
+                          'In-depth Insights, Meticulous Analysis',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: AppColors.textColor,
+                            fontSize: 12,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
