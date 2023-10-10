af41f5cc663ece5612d5428a6092a55b78cedd85
Venuja <93823328+VenujaK@users.noreply.github.com>
2023-10-10 20:08:35 +0530

    Survey updated need to check the connection

--- lib/Pages/survey.dart
+++ lib/Pages/survey.dart
@@ -9,13 +9,73 @@ class SurveyPage extends StatefulWidget {
 }
 
 class _SurveyPageState extends State<SurveyPage> {
-  TextEditingController sadController = TextEditingController();
-  TextEditingController interestController = TextEditingController();
-  TextEditingController appetiteController = TextEditingController();
-  TextEditingController sleepController = TextEditingController();
-  TextEditingController energyController = TextEditingController();
-  TextEditingController concentrationController = TextEditingController();
-  TextEditingController worthlessnessController = TextEditingController();
+  int userAnswer1 = -1;
+  int userAnswer2 = -1;
+  int userAnswer3 = -1;
+  int userAnswer4 = -1;
+  int userAnswer5 = -1;
+  int userAnswer6 = -1;
+  int userAnswer7 = -1;
+
+ void sendAnswers() async {
+  if (userAnswer1 != -1 &&
+      userAnswer2 != -1 &&
+      userAnswer3 != -1 &&
+      userAnswer4 != -1 &&
+      userAnswer5 != -1 &&
+      userAnswer6 != -1 &&
+      userAnswer7 != -1) {
+    var url = Uri.parse('http://127.0.0.1:5001/predict');
+
+    var headers = {'Content-Type': 'application/json'}; // Set content type to JSON
+
+    Map<String, dynamic> answers = {
+      'answer1': userAnswer1,
+      'answer2': userAnswer2,
+      'answer3': userAnswer3,
+      'answer4': userAnswer4,
+      'answer5': userAnswer5,
+      'answer6': userAnswer6,
+      'answer7': userAnswer7,
+    };
+
+    var response = await http.post(
+      url,
+      headers: headers,
+      body: json.encode(answers), // Convert data to JSON
+    );
+
+    if (response.statusCode == 200) {
+      var data = jsonDecode(response.body);
+      String result = data['result'];
+
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Predicted Result'),
+            content: Text('The predicted result is: $result'),
+            actions: <Widget>[
+              TextButton(
+                child: Text('Close'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        },
+      );
+    } else {
+      // Handle errors
+      print('Error: ${response.reasonPhrase}');
+    }
+  } else {
+    // Handle case where user hasn't answered all questions
+    print('Please answer all questions');
+  }
+}
+
 
   @override
   Widget build(BuildContext context) {
@@ -23,140 +83,161 @@ class _SurveyPageState extends State<SurveyPage> {
       appBar: AppBar(
         title: Text('Survey'),
       ),
-      body: SingleChildScrollView(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text(
-              '1. Have you been feeling consistently sad or down for most of the day, nearly every day, for at least two weeks?',
-            ),
-            _buildYesNoField(sadController),
-            SizedBox(height: 10),
-            Text(
-              '2. Have you lost interest or pleasure in activities that you used to enjoy?',
-            ),
-            _buildYesNoField(interestController),
-            SizedBox(height: 10),
-            Text(
-              '3. Have you experienced significant changes in your appetite or weight recently (significant increase or decrease)?',
-            ),
-            _buildYesNoField(appetiteController),
-            SizedBox(height: 10),
-            Text(
-              '4. Are you having trouble sleeping or sleeping excessively?',
-            ),
-            _buildYesNoField(sleepController),
-            SizedBox(height: 10),
-            Text(
-              '5. Do you feel unusually fatigued or lacking in energy, even when you\'ve had enough sleep?',
-            ),
-            _buildYesNoField(energyController),
-            SizedBox(height: 10),
-            Text(
-              '6. Do you find it difficult to concentrate, make decisions, or remember things?',
-            ),
-            _buildYesNoField(concentrationController),
-            SizedBox(height: 10),
-            Text(
-              '7. Have you experienced feelings of worthlessness or excessive guilt?',
-            ),
-            _buildYesNoField(worthlessnessController),
-            SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: _submitForm,
-              child: Text('Submit'),
-            ),
-          ],
-        ),
+      body: Column(
+        children: <Widget>[
+          Text('Question 1: Have you been feeling sad?'),
+          Row(
+            children: <Widget>[
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    userAnswer1 = 1;
+                  });
+                },
+                child: Text('Yes'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    userAnswer1 = 0;
+                  });
+                },
+                child: Text('No'),
+              ),
+            ],
+          ),
+          Text('Question 2: Have you lost interest or pleasure in activities?'),
+          Row(
+            children: <Widget>[
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    userAnswer2 = 1;
+                  });
+                },
+                child: Text('Yes'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    userAnswer2 = 0;
+                  });
+                },
+                child: Text('No'),
+              ),
+            ],
+          ),
+          Text('Question 3: Have you experienced changes in appetite or weight?'),
+          Row(
+            children: <Widget>[
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    userAnswer3 = 1;
+                  });
+                },
+                child: Text('Yes'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    userAnswer3 = 0;
+                  });
+                },
+                child: Text('No'),
+              ),
+            ],
+          ),
+          Text('Question 4: Are you having trouble sleeping?'),
+          Row(
+            children: <Widget>[
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    userAnswer4 = 1;
+                  });
+                },
+                child: Text('Yes'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    userAnswer4 = 0;
+                  });
+                },
+                child: Text('No'),
+              ),
+            ],
+          ),
+          Text('Question 5: Do you feel unusually fatigued or lacking in energy?'),
+          Row(
+            children: <Widget>[
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    userAnswer5 = 1;
+                  });
+                },
+                child: Text('Yes'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    userAnswer5 = 0;
+                  });
+                },
+                child: Text('No'),
+              ),
+            ],
+          ),
+          Text('Question 6: Do you find it difficult to concentrate or make decisions?'),
+          Row(
+            children: <Widget>[
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    userAnswer6 = 1;
+                  });
+                },
+                child: Text('Yes'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    userAnswer6 = 0;
+                  });
+                },
+                child: Text('No'),
+              ),
+            ],
+          ),
+          Text('Question 7: Have you experienced feelings of worthlessness or guilt?'),
+          Row(
+            children: <Widget>[
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    userAnswer7 = 1;
+                  });
+                },
+                child: Text('Yes'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    userAnswer7 = 0;
+                  });
+                },
+                child: Text('No'),
+              ),
+            ],
+          ),
+          ElevatedButton(
+            onPressed: sendAnswers,
+            child: Text('Submit Answers'),
+          ),
+        ],
       ),
-    );
-  }
-
-  Widget _buildYesNoField(TextEditingController controller) {
-    return Row(
-      children: <Widget>[
-        Radio(
-          value: 'yes',
-          groupValue: controller.text,
-          onChanged: (value) {
-            setState(() {
-              controller.text = value!;
-            });
-          },
-        ),
-        Text('Yes'),
-        Radio(
-          value: 'no',
-          groupValue: controller.text,
-          onChanged: (value) {
-            setState(() {
-              controller.text = value!;
-            });
-          },
-        ),
-        Text('No'),
-      ],
-    );
-  }
-
-  void _submitForm() async {
-    Map<String, int> answers = {
-      'sad': _parseAnswer(sadController.text),
-      'interest': _parseAnswer(interestController.text),
-      'appetite': _parseAnswer(appetiteController.text),
-      'sleep': _parseAnswer(sleepController.text),
-      'energy': _parseAnswer(energyController.text),
-      'concentration': _parseAnswer(concentrationController.text),
-      'worthlessness': _parseAnswer(worthlessnessController.text),
-    };
-
-    int prediction = await _sendToServer(answers);
-
-    _showResultDialog(prediction);
-  }
-
-  int _parseAnswer(String answer) {
-    return answer == 'yes' ? 1 : 0;
-  }
-
-  Future<int> _sendToServer(Map<String, int> answers) async {
-    final response = await http.post(
-      Uri.parse('http://10.0.2.2:5000/predict'),
-      headers: <String, String>{
-        'Content-Type': 'application/json; charset=UTF-8',
-      },
-      body: jsonEncode({'inputs': answers}),
-    );
-
-    print('Response status code: ${response.statusCode}');
-    print('Response body: ${response.body}');
-
-    if (response.statusCode == 200) {
-      Map<String, dynamic> data = json.decode(response.body);
-      return data['predicted_value'];
-    } else {
-      throw Exception('Failed to get prediction');
-    }
-  }
-
-  void _showResultDialog(int prediction) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Prediction Result'),
-          content: Text('The predicted value is: $prediction'),
-          actions: <Widget>[
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: Text('Close'),
-            ),
-          ],
-        );
-      },
     );
   }
 }
